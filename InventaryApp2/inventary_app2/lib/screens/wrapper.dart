cdd9585f2b36d0189a9ef56521eeeb32e9e631cc
Joel <joelserranoservera12@gmail.com>
2020-02-04 11:21:17 +0100

    Firestore migration
    New Server location

--- InventaryApp2/inventary_app2/lib/screens/wrapper.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-import 'package:inventary_app2/screens/authenticate/authenticate.dart';
-
-class Wrapper extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Authenticate();
-  }
-}
