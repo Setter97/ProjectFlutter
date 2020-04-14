f0fe6bdb524361c812127d2f99dab93bca8b92d7
Joel <joelserranoservera12@gmail.com>
2020-04-14 12:15:13 +0200

    Login hecho y a demas to bonito mah quedao

--- test_db/lib/screens/SignInScreen.dart
+++ test_db/lib/screens/SignInScreen.dart
@@ -34,7 +34,7 @@ class _LoginPageState extends State<LoginPage> {
             mainAxisSize: MainAxisSize.max,
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
-              FlutterLogo(size: 150),
+              Image(image: AssetImage("assets/logo.png"), height: 200.0),
               SizedBox(height: 50),
               _signInButton(),
             ],
